acdfa70bb0fdc3f4a0ae6efd8ca0fb092502e3b5
arlindaislami <arlindaaislamii@gmail.com>
2025-03-24 14:32:30 +0100

    Get started UI

--- app/src/main/java/com/example/togetherwecan/MainActivity.kt
+++ app/src/main/java/com/example/togetherwecan/MainActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.togetherwecan.ui.theme.TogetherWeCanTheme
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,21 +45,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val database = Firebase.database
-    val myRef = database.getReference("test_connection")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.togetherwecanlogo),
+            contentDescription = "Logo User",
+            modifier = Modifier
+                .size(80.dp)
+                .padding(bottom = 16.dp)
+        )
 
 
         Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = "Together We Can",
+            fontSize = 30.sp,
+            style = MaterialTheme.typography.headlineLarge
         )
 
-    Button(onClick = {
-        myRef.setValue("test test!")
-    }) {
-        Text("SAVE")
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        Text(
+            text = "Find volunteer opportunities near you and help those in need.",
+            fontSize = 16.sp
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+
+        Button(onClick = {  }, modifier = Modifier.fillMaxWidth(0.6f)) {
+            Text(text = "Log In")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        Button(onClick = { }, modifier = Modifier.fillMaxWidth(0.6f)) {
+            Text(text = "Sign Up")
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
