5e27ed4cddff064b34bd0a2edd7f830941160626
arlindaislami <arlindaaislamii@gmail.com>
2025-04-21 12:47:36 +0200

    Fix Problem

--- app/src/main/java/com/example/togetherwecan/MyEventsVolunter.kt
+++ app/src/main/java/com/example/togetherwecan/MyEventsVolunter.kt
@@ -3,23 +3,11 @@ package com.example.togetherwecan
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,9 +15,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun MyEventsVolunterScreen(viewModel: EventsViewModel = viewModel()) {
@@ -93,7 +81,8 @@ fun MyEventsVolunterScreen(viewModel: EventsViewModel = viewModel()) {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(vertical = 8.dp),
-                        elevation = 4.dp
+                        colors = CardDefaults.cardColors(containerColor = Color.White),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
                     ) {
                         Column(modifier = Modifier.padding(16.dp)) {
                             Text(event.eventTitle, fontWeight = FontWeight.Bold, fontSize = 18.sp)
@@ -118,10 +107,3 @@ fun MyEventsVolunterScreen(viewModel: EventsViewModel = viewModel()) {
         }
     }
 }
-
-@Preview (showBackground = true)
-@Composable
-fun MyEventsVolunterScreenPreview() {
-    MyEventsVolunterScreen()
-}
-
